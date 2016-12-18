386711531069412f52376fbefbc136bf0bf70563
Frangky Tumiwan <frangky.tumiwan@gmail.com>
2016-12-18 08:49:02 +0800

    scaffold for generate routes.php script

--- src/Routes.php
+++ src/Routes.php
@@ -1,1 +1,11 @@
 <?php
+use Illuminate\Http\Request;
+use FTumiwan\DomainCore\Gate;
+
+//*--- begin FakturPenjualan
+
+	    Route::post('api/faktur-penjualan', function (Request $request){
+			return (new Gate((new App\Domain\FakturPenjualan\Context())))->httpComing($request);
+		});
+	    
+//*--- end FakturPenjualan
--- src/core/command/Scaffold.php
+++ src/core/command/Scaffold.php
@@ -26,6 +26,7 @@ class Scaffold
 		$this->makeFactorySignal();
 		$this->makeContext();
 		$this->checkingBridgeFile();
+		$this->makeRoute();
 	}
 
 	public function update() {
@@ -196,6 +197,49 @@ class Scaffold
 
 	    return true;
   }
+
+  public function contentManipulation($text_file,$data,$flag) { //text_file: text file, data: content inserted, flag: identify begin to end
+		$m = "";
+		$_content = explode("\n",$text_file);
+		$_domainFlagOpen = "//*--- begin ".$flag;
+		$_domainFlagClose = "//*--- end ".$flag;
+		$new = true;
+		$remove = false;
+		$content = "";
+		while (list($var,$val) = each($_content)) {
+			++$var;
+			$val = trim($val);		
+			if ($val==$_domainFlagOpen) {				
+				$remove = true; 
+			} else {
+				if ($val==$_domainFlagClose) {
+					$remove = false;
+					continue;
+				}
+			}
+			if ($remove==false) {
+				$content .= $val."\n";
+			}
+		}
+		$content .= $_domainFlagOpen."\n".$data."\n".$_domainFlagClose;		
+		return $content;
+	}
+
+
+	public function makeRoute() {
+	    $_route = "
+	    Route::post('api/%routename%', function (Request \$request){
+			return (new Gate((new App\\Domain\\%domainname%\\Context())))->httpComing(\$request);
+		});
+	    ";
+	    $_domain = str_replace('_','-',snake_case($this->domainname));
+	    $_path = base_path('packages/ftumiwan/domaincore/src');
+	    $webroute = file_get_contents($_path."/Routes.php");
+	    $r_data = str_replace("%routename%",$_domain,$_route);
+	    $r_data_ = str_replace("%domainname%",$this->domainname,$r_data);
+	    $routeContent = $this->contentManipulation($webroute,$r_data_,$this->domainname);	   
+	    file_put_contents($_path."/Routes.php",$routeContent);
+  	}
 
 
 
