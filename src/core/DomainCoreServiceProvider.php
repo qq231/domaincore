ecfe571cf94ef01ba28b8bbb2fd64515e1604cff
Frangky Tumiwan <frangky.tumiwan@gmail.com>
2016-12-21 16:32:38 +0800

    bugfixed routes

--- src/core/DomainCoreServiceProvider.php
+++ src/core/DomainCoreServiceProvider.php
@@ -14,7 +14,9 @@ class DomainCoreServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../Routes.php');        
+        if (file(__DIR__.'/../Routes.php')) {
+            $this->loadRoutesFrom(__DIR__.'/../Routes.php');        
+        }        
         $this->publishes([
             __DIR__.'/../../schema' => base_path('packages/ftumiwan/schema'),
         ]);
