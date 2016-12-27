e589f1e3a88419aed212301ed49afc06a903c08b
Frangky Tumiwan <frangky.tumiwan@gmail.com>
2016-12-27 13:44:55 +0800

    bug field name

--- src/core/Repo.php
+++ src/core/Repo.php
@@ -48,7 +48,7 @@ class Repo
     $m = $this->entity;
     $qr = $m::select();
     foreach($fill as $key=>$val) {
-      $qr->where($key['field'],$key['operator'],$key['val']);
+      $qr->where($key['fl'],$key['opr'],$key['vl']);
     }
     return $qr->get();
   }
